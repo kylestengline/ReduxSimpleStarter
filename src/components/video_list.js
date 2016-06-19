3ef496807efc9c357e9a8f649d051e5736a061cb
Kyle Stengline <kyle.stengline@gmail.com>
2016-06-19 17:06:42 -0400

    adds code for displaying video thumbnails and description

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,9 +3,9 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />  
+    return <VideoListItem key={video.etag} video={video} />  
   }); 
-
+//going through each video and providing it with a key known as an etag(element tag). this means that each video has a key so that react can update the appropriate record in a speedy fashion.
   return (
     <ul className="col-md-4 list-group">
       { videoItems }
